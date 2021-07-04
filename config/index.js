25456bef9fcd03fac41645776b2a11ada8242cab
ryderwishart <ryderwishart@gmail.com>
2021-07-03 21:29:11 -0700

    fix(config): fixes server path for production

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-// export const server = dev
-//   ? 'http://localhost:9999'
-//   : 'https://lsj.vercel.app/';
+export const server = dev
+  ? 'http://localhost:9999'
+  : 'https://lsj.vercel.app/';
 
-export const server = 'http://localhost:9999'
+// export const server = 'http://localhost:9999'
 // export const server = 'https://opentext.vercel.app/'
